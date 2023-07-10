796f6cbc3e792472b07f77dff60443e2a8b3042d
GabrielaSmolen <gabrysiasmolen@gmail.com>
2023-07-10 23:45:18 +0200

    Add docstring

--- extract_labels.py
+++ extract_labels.py
@@ -2,7 +2,12 @@ import pandas as pd
 from pathlib import Path
 
 
-def extract_labels(file_path):
+def extract_labels(file_path: Path) -> pd.DataFrame:
+    """
+    Extract labels from text files.
+    :param file_path: Path to txt files
+    :return: extracted labels including start time, end time, wav file name, emotion, val, act, dom
+    """
     extracted_labels = pd.DataFrame(columns=["start_time", "end_time", "wav_file", "emotion", "val", "act", "dom"])
     with open(file_path) as file:
         for line in file:
@@ -23,4 +28,3 @@ if __name__ == '__main__':
         for path in list(paths):
             labels = extract_labels(path)
             all_labels = pd.concat([all_labels, labels], ignore_index=True)
-    print()
